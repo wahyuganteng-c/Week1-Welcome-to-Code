fa4df3418daf15cdc9242723f3c77dbaffc09499
wahyuganteng-c <3yuuwahyu@gmail.com>
2025-04-10 22:45:52 +0700

    submit quiz UJIAN week 1

---
+++ quiz/ujian/jawabansoal4.js
@@ -0,0 +1,25 @@
+//   Diberikan sebuah function xo(str) yang menerima satu parameter berupa string. 
+//   Function akan me-return true jika jumlah karakter x sama dengan jumlah karakter o, dan false jika tidak.
+
+function xo(str) {
+
+    let x = 0, o = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        if (char === 'x') {
+          x++;
+        } else if (char === 'o') {
+          o++;
+        }
+    }
+    return x === o;
+      
+}
+  
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
